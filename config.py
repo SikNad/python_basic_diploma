a00018fae1e496b22b62353a648b4f09ef4970fe
nadezhda_sikorskaia <nadya.sikorskaya@yandex.ru@skillbox.ru>
2026-06-11 16:50:33 +0500

    Debug token

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 API_KEY = os.getenv('API_KEY')
 DB_PATH = os.getenv('DB_PATH', 'database/movies.db')
 DATE_FORMAT = os.getenv('DATE_FORMAT', '%d.%m.%Y')
+
+print(f"DEBUG TOKEN: {BOT_TOKEN[:20] if BOT_TOKEN else 'NONE'}")
